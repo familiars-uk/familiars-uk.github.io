94349da1aaad35c670445503495753457d92d077
John Else <john.else@gmail.com>
2017-09-13 22:15:31 +0100

    Add contact form
    Signed-off-by: John Else <john.else@gmail.com>

---
+++ js/contact.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+    $("#contact").on("submit", function(e) {
+        e.preventDefault();
+        $("#submit").prop("disabled", true);
+        $("#submit").val("...");
+
+        var target = "aHR0cHM6Ly9mb3Jtc3ByZWUuaW8vZmFtaWxpYXJzLmNhbWJyaWRnZUBnbWFpbC5jb20=";
+
+        var name = $("#name").val();
+        var email = $("#email").val();
+        var comments = $("#comments").val();
+
+        $.ajax({
+            url: atob(target),
+            method: "POST",
+            data:{
+                name: name,
+                _replyto: email,
+                email: email,
+                message: message,
+                _subject: "Form submission",
+            },
+            dataType: "json",
+            success: function() {
+                $("#form").hide();
+                $("#thanks").show();
+            }
+        });
+    });
+});
