a3b2bcd8cd72033a5709786535919ade5433d451
John Else <john.else@gmail.com>
2017-10-10 22:26:38 +0100

    Implement photo gallery
    Signed-off-by: John Else <john.else@gmail.com>

---
+++ js/gallery.js
@@ -0,0 +1,39 @@
+var images = [
+    //"IMG2_5156",
+    "IMG2_5157",
+    //"IMG2_5167",
+    "IMG2_5190",
+    "IMG2_5195",
+    //"IMG2_5196",
+    "IMG2_5197",
+    //"IMG2_5198",
+    "IMG2_5220",
+    "IMG2_5222",
+    "IMG2_5226",
+    //"IMG2_5228",
+    //"IMG2_5234",
+];
+
+var imageIndex = 0;
+
+function mod(number, divisor) {
+    return ((number % divisor) + divisor) % divisor;
+}
+
+function showImage(index) {
+    var html = "<img src=\"images/gallery/" + images[index] + ".jpg\">";
+    $("#photo").html(html);
+}
+
+$(document).ready(function() {
+    $("#bck").on('click', function() {
+        imageIndex = mod(imageIndex - 1, images.length);
+        showImage(imageIndex);
+    });
+    $("#fwd").on('click', function() {
+        imageIndex = mod(imageIndex + 1, images.length);
+        showImage(imageIndex);
+    });
+
+    showImage(imageIndex);
+});
